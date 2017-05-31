65888ff951809efce9e959b3bc1a79a71582ae85
ViktorKovalev3 <ya72480@gmail.com>
2017-05-31 17:03:57 +0300

    write Engine class + implementation

--- Course/Engine.cpp
+++ Course/Engine.cpp
@@ -1,6 +1,52 @@
 #include "Engine.h"
 
-Engine::Engine()
+Engine::Engine(QObject* parent) :
+    QObject(parent),
+    timer(new QTimer(this)),
+    modelOfManufactory_(
+        std::make_unique<Model>(
+            DEFAULT_NUM_OF_MECH, DEFAULT_NUM_OF_MACH,
+            DEFAULT_MACH_FAIL_MEAN, DEFAULT_MECH_FAIL_MEAN
+            )
+        )
 {
+    connect(timer, SIGNAL(timeout()), this, SLOT(doSecndStepInModel()));
+}
 
+void Engine::start(minutes minutePerSecnd, minutes stopTime)
+{
+    timer->start(1000 / minutePerSecnd); //1000 ms = 1 sec
+    stopTime_ = stopTime;
+}
+
+void Engine::pause()
+{
+    timer->stop();
+}
+
+void Engine::stop()
+{
+    timer->stop();
+    modelOfManufactory_->resetModel();
+}
+
+void Engine::setSettings(uint numOfMechanics, uint numOfMachines, double machinesFailuresMean, double mechanicsRepairingTimeMean)
+{
+    modelOfManufactory_->setNumOfMechanics(numOfMechanics);
+    modelOfManufactory_->setNumOfMachines(numOfMachines);
+    modelOfManufactory_->setMachinesFailuresMean(machinesFailuresMean);
+    modelOfManufactory_->setMechanicsRepairingTimeMean(mechanicsRepairingTimeMean);
+}
+
+void Engine::doSecndStepInModel()
+{
+    minute modelTime = modelOfManufactory_->doMinuteStepAndGiveModelTime();
+    if (modelTime <= stopTime_)
+        emit sendModelResults(
+                modelOfManufactory_->getNumOfBusyMechanics(),
+                modelOfManufactory_->getNumOfBrokenMachines(),
+                modelTime
+                );
+    else
+        stop();
 }
--- Course/Engine.h
+++ Course/Engine.h
@@ -2,26 +2,40 @@
 #define ENGINE_H
 
 #include <QObject>
+#include <QTimer>
+#include <memory>
 #include "Model.h"
 
 typedef unsigned uint;
 typedef uint minutes;
 
-class Engine
+#define DEFAULT_MIN_PER_SCND 1
+#define DEFAULT_STOP_SCND 10000
+#define DEFAULT_NUM_OF_MECH 2
+#define DEFAULT_NUM_OF_MACH 20
+#define DEFAULT_MACH_FAIL_MEAN 1
+#define DEFAULT_MECH_FAIL_MEAN 60
+
+class Engine : public QObject
 {
+    Q_OBJECT
 public:
-    Engine();
-    ~Engine();
+    Engine(QObject* parent = nullptr);
 public slots:
-    void start ( minutes minutePerSecnd, minutes stopTime );
+    void start ( minutes minutePerSecnd = DEFAULT_MIN_PER_SCND, minutes stopTime = DEFAULT_STOP_SCND);
     void pause ();
     void stop ();
-    void setSettings ( uint numOfMechanics, uint numOfMachines,
-                       double machinesFailuresMean, double mechanicsRepairingTimeMean );
+    void setSettings ( uint numOfMechanics = DEFAULT_NUM_OF_MECH, uint numOfMachines = DEFAULT_NUM_OF_MACH,
+                       double machinesFailuresMean = DEFAULT_MACH_FAIL_MEAN, double mechanicsRepairingTimeMean = DEFAULT_MECH_FAIL_MEAN);
+private slots:
+    void doSecndStepInModel();
 signals:
     void sendModelResults ( uint numOfBisyMechanics, uint numOfBrokenMachines, uint modelTime );
 private:
-    Model* manufactoryModel_;
+    QTimer* timer;
+    minutes stopTime_;
+    bool isPaused = 0;
+    std::unique_ptr<Model> modelOfManufactory_;
 };
 
 #endif // ENGINE_H
